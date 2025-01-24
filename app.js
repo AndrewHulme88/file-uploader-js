277f79ccf005187f7645ff6cfbd427ac7719227a
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-24 14:37:25 +1100

    Created and generated prisma schema

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
 
 app.listen(PORT, () => console.log("App listening on port 3000"));
